54d2051d88767425b7c2c7c359ad934d2211c658
Sora Tonami <ms0503@outlook.com>
2023-02-25 22:32:34 +0900

    ビルドスクリプトを修正。

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ yarn install --frozen-lockfile || exit $?
 cd "$BASE_DIR/node_modules/eslint-config-eslint"
 rm default.yml
 wget https://raw.githubusercontent.com/eslint/eslint/main/packages/eslint-config-eslint/default.yml
-cd "$BASE_DIR/node_modules/@types/react"
-rm index.d.ts
-wget https://raw.githubusercontent.com/ms0503/DefinitelyTyped/fix/react-styled-jsx/types/react/index.d.ts
 cd "$BASE_DIR"
 yarn lint || exit $?
 yarn build || exit $?
