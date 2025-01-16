a4a0009769240f2959b8f4482979ba4a30035391
ms0503 <ms0503@outlook.com>
2025-01-16 16:51:14 +0900

    ruby環境のdevShellを追加

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              bundler
+              ruby
+            ];
+          };
+        };
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    };
+}
